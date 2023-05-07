99f3540973ceae479fde188ba6fb90932726b084
BeaComp <102777924+BeaComp@users.noreply.github.com>
2023-05-07 19:00:21 -0300

    Implemented the modal and API of the phrase of the day

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+// Dark Mode
+
 const darkMode = () => {
     const botaoInterno = document.getElementById("radio-inner")
     var img = document.querySelector("#image")
@@ -9,14 +11,17 @@ const darkMode = () => {
     const body = document.querySelector("body")
 
     const frasedodia = document.querySelector("#btnfrase")
+    const fraseMobile = document.querySelector("#btnfraseMobile")
+    const modal = document.querySelector("#modal")
 
     var imgIcone = document.querySelector("#imageIcone")
-    
+    var imgIconeMobile = document.querySelector("#imageIconeMobile")
+    var imgIconeModal = document.querySelector("#imageIconeModal")
 
     const footer = document.querySelector("footer")
 
 
-    if(botaoInterno.classList.contains("active")) {
+    if (botaoInterno.classList.contains("active")) {
         botaoInterno.classList.remove("active")
         img.src = "img/light_mode.svg"
         header.classList.remove("dark")
@@ -24,8 +29,12 @@ const darkMode = () => {
         body.style.backgroundColor = "#FFFFFF"
         footer.style.backgroundColor = "#114FA7"
         frasedodia.classList.remove("dark")
+        fraseMobile.classList.remove("dark")
         imgIcone.src = "img/Ícone.svg"
-        
+        imgIconeMobile.src = "img/Ícone.svg"
+        modal.classList.remove("dark")
+        imgIconeModal.src = "img/Ícone.svg"
+
 
     } else {
         botaoInterno.classList.add("active")
@@ -35,14 +44,18 @@ const darkMode = () => {
         body.style.backgroundColor = "#222222"
         footer.style.backgroundColor = "#111111"
         frasedodia.classList.add("dark")
+        fraseMobile.classList.add("dark")
         imgIcone.src = "img/Icone_Dark.svg"
-        
-        
+        imgIconeMobile.src = "img/Icone_Dark.svg"
+        imgIconeModal.src = "img/Icone_Dark.svg"
+        modal.classList.add("dark")
+
+
 
     }
 
     const botaoExterno = document.getElementById("radio-btn")
-    if(botaoExterno.classList.contains("fundo")) {
+    if (botaoExterno.classList.contains("fundo")) {
         botaoExterno.classList.remove("fundo")
     } else {
         botaoExterno.classList.add("fundo")
@@ -50,6 +63,59 @@ const darkMode = () => {
 
 }
 
-
 const botaoDarkmode = document.getElementById("radio-btn")
 botaoDarkmode.onclick = darkMode
+
+//Modal do Mobile
+
+const mostrarModal = () => {
+    const modal = document.querySelector(".fundo-modal")
+    modal.classList.add("mostrar")
+}
+
+const abrirModal = document.querySelector(".fraseMobile")
+abrirModal.onclick = mostrarModal
+
+const esconderModal = () => {
+    const modal = document.querySelector(".fundo-modal")
+    modal.classList.remove("mostrar")
+}
+
+const fecharModal = document.querySelector(".btnfechar")
+fecharModal.onclick = esconderModal
+
+//API frase do Dia
+
+const elementoErro = document.getElementById("erro");
+const elementoErroMobile = document.getElementById("erroMobile");
+const p = document.querySelector("#frase-paragrafo");
+const pMobile = document.querySelector("#frase-paragrafoMobile");
+
+
+
+fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then(data => {
+        const fraseOriginal = data.slip.advice;
+        const url = `https://api.mymemory.translated.net/get?q=${fraseOriginal}&langpair=en|pt&key=${'5be2f48704466091a687'}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const fraseTraduzida = data.responseData.translatedText;
+                p.innerText = fraseTraduzida;
+                pMobile.innerText = p.innerText = fraseTraduzida;
+            })
+            .catch(error => {
+                elementoErro.innerText = "Ocorreu um erro ao traduzir a frase aleatória.";
+                elementoErroMobile.innerText = "Ocorreu um erro ao traduzir a frase aleatória.";
+            });
+    })
+    .catch(error => {
+        elementoErro.innerText = "Ocorreu um erro ao obter a frase aleatória.";
+        elementoErroMobile.innerText = "Ocorreu um erro ao obter a frase aleatória.";
+    });
+
+
+
+
